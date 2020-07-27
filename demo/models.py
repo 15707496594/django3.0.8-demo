eda22327c399ffbd9b4b0705a5b7a809872a4701
邹宇航 <1114037639@qq.com>
2020-07-27 16:32:35 +0800

    引入 django-rest-framework

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Demo(models.Model):
+    name = models.CharField(max_length=100, blank=True, default='')
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('created_time', )
